f0b98000690770a2919a5d6d39cc08230d706d3a
TheRenegadeOfFunk <therenegadeoffunk@gmail.com>
2016-10-01 16:15:42 +0900

    Finished initial data generator
    Okay this is pretty quick and dirty, but she works.
    
    Executing this bad boy inserts random entries into the table.
    Thanks to the use of the 'words.txt' file, the names of the
    games are pretty amazing. The 'score' and 'budget' parameters
    are explicitly made to correlate for the purpose of testing
    learning algorithms.
    
    Eventually I'll generify it more - maybe use a conf file for
    arbitrary tables / keyspaces, maybe take command line arguments.
    For my next trick, I just need to get it to execute several
    hundred times.

--- Python/cassandra_generate.py
+++ Python/cassandra_generate.py
@@ -1,11 +1,51 @@
 #!/usr/bin/env python
 
 from cassandra.cluster import Cluster
+import random
 
-cluster = Cluster()
+words = open('words.txt').read().splitlines()
 
-session = cluster.connect('games')
+def setup():
+    cluster = Cluster()
+    session = cluster.connect('games')
+    return session
 
-output = session.execute('SELECT * FROM game_by_name')
+def get_score(budget):
+    if budget < 25:
+        score = random.randrange(10, 101)
+        return score
+    elif budget <= 50:
+        score = random.randrange(25, 101)
+        return score
+    elif budget <= 75:
+        score = random.randrange (45, 101)
+        return score
+    else:
+        score = random.randrange (60, 101)
+        return score
 
-print  output[0]
+def get_params():
+    title = make_title()
+    budget = random.randrange(1, 101)
+    score = get_score(budget)
+    return [ title, score, budget ]
+
+def make_title():
+    title_length = random.randrange(1, 7)
+    terms = []
+    i = 0
+    while i <= title_length:
+        terms.append(random.choice(words))
+        i += 1
+    title = ' '.join(terms)
+    return title
+
+def generate_data(session):
+    params = get_params()
+    query = '''INSERT INTO game_by_name ( name, score, budget )
+            VALUES ( %s, %s, %s );'''
+    session.execute(query, (params[0], params[1], params[2]))
+
+if __name__ == "__main__":
+    session = setup()
+    generate_data(session)
