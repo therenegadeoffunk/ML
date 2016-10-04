03218cc3f8062a1167340e26e116209b3b01ca5d
TheRenegadeOfFunk <therenegadeoffunk@gmail.com>
2016-10-04 11:21:02 +0900

    Added functions for Postgres data generator in R
    Not quite done but the structure is there. I am not sure about
    the method for generating random character strings. Seemed kind
    of slow in the REPL. Once I can get the function for determining
    malignancy worked out, I'll give it a spin in a VM and see if
    we don't need a different approach.

--- R/postgres-generate.r
+++ R/postgres-generate.r
@@ -1,15 +1,28 @@
 #!/usr/bin/Rscript
 
 require('RPostgreSQL')
+require('random')
 
-testFunc <- function()
+setup <- function()
 {
     drv <- dbDriver('PostgreSQL')
     con <- dbConnect(drv, user='postgres', dbname='cancer')
-    resp <- dbGetQuery(con, "SELECT * FROM tumors")
-    print(resp)
-    dbDisconnect(con)
-    dbUnloadDriver(drv)
+    return(con)
 }
 
-testFunc()
+make_parameters <- function()
+{
+    id <- randomStrings(1, len=20)
+    size <- runif(1)
+    # Need some method for deciding malignancy based on size
+}
+
+insert_data -< function(connection, parameters)
+{
+    resp <- dbGetQuery(con, "SELECT * FROM tumors")
+}
+
+con <- setup()
+param <- make_parameters()
+insert_data(con, param)
+print("All done!")
