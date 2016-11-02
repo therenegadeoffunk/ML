48bf34c7071c5487870035e236669e841fd26002
TheRenegadeOfFunk <therenegadeoffunk@gmail.com>
2016-11-02 14:49:03 +0900

    Added CSV functionality to profiler
    For now it only works for one table. I need to add another table
    to my test Cassandra keyspace, then have it iterate over the
    query result object.

--- Python/Cassandra/profile.py
+++ Python/Cassandra/profile.py
@@ -2,6 +2,7 @@
 
 from cassandra.cluster import Cluster
 import sys
+import csv
 
 keyspace_name = sys.argv[1]
 
@@ -14,16 +15,19 @@ def profile_tables(session, keyspace_name):
     query = '''SELECT * FROM system_schema.tables \
             WHERE keyspace_name=%s;'''
     result = session.execute(query, (keyspace_name,))
-    # Just printing for now. We'll do cooler things later
-    for item in result:
-        print item
+    # Spit out a CSV. This only works for a single table keyspace. Will fix.
+    name = result[0].keyspace_name + ".csv"
+    table_dict = result[0].__dict__
+    with open(name, 'wb') as f:
+        w = csv.writer(f)
+        w.writerows(table_dict.items())
+    f.close()
 
 # This method for profiling an individual table may be useful later    
 def profile_table(session, keyspace_name, table):
     query = '''SELECT * FROM system_schema.tables WHERE \
             keyspace_name=%s AND table_name=%s;'''
     result = session.execute(query, (keyspace_name, table))
-    # Just printing for now
     print result[0]
 
 if __name__ == "__main__":
