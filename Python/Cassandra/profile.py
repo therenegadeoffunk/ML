b059c86446efbc2af01f1dc30c490f628ac43515
TheRenegadeOfFunk <therenegadeoffunk@gmail.com>
2016-11-03 15:22:48 +0900

    Refactored profile.py and added functionality
    We are going to continue outputting small csv's for each
    table for the time being. This commit breaks up the code
    into more functions and sets up some additional data
    gathering. Specifically we have a method for getting the
    table size on disk and a query for getting the fields
    (columns).
    
    Once we munge it all together, we'll have the schema, size,
    and columns for every table in its own small csv. From there
    we can export to a Google doc, do automated diffing, or
    other fun stuff.

--- Python/Cassandra/profile.py
+++ Python/Cassandra/profile.py
@@ -3,36 +3,62 @@
 from cassandra.cluster import Cluster
 import sys
 import csv
+import os
 
-keyspace_name = sys.argv[1]
+if len(sys.argv) < 2:
+    keyspace_name = "games"
+else:
+    keyspace_name = sys.argv[1]
+
+schema_query = '''SELECT * FROM system_schema.tables \
+                WHERE keyspace_name=%s;'''
+
+columns_query = '''SELECT keyspace_name, table_name, column_name \
+                FROM system_schema.columns WHERE keyspace_name=%s'''
+
+du_command = "echo 'size_on_disk',`du -hc /var/lib/cassandra/data/%s/%s* \
+            | grep total | awk '{print $1}'`"
 
 def setup():
     cluster = Cluster()
     session = cluster.connect()
     return session
 
-def profile_tables(session, keyspace_name):
-    query = '''SELECT * FROM system_schema.tables \
-            WHERE keyspace_name=%s;'''
-    result = session.execute(query, (keyspace_name,))
-    dicts = []
-    for a in result:
-        a = a.__dict__
-        dicts.append(a)
-    for d in dicts:       
+def generate_csv(dlist):
+    for d in dlist:
         name = d['table_name'] + ".csv"
         with open(name, 'wb') as f:
             w = csv.writer(f)
             w.writerows(d.items())
         f.close()
 
-# This method for profiling an individual table may be useful later    
-def profile_table(session, keyspace_name, table):
-    query = '''SELECT * FROM system_schema.tables WHERE \
-            keyspace_name=%s AND table_name=%s;'''
-    result = session.execute(query, (keyspace_name, table))
-    print result[0]
+def add_table_size(dicts):
+    # Using os instead of subprocess because I miss 2.6...
+    for thing in dicts:
+        out = os.popen(du_command % (thing['keyspace_name'], thing['table_name']))
+        a = out.read().rstrip()
+        vals = a.split(",")
+        thing[vals[0]] = vals[1]
+    return dicts
+
+def get_table_schemas(session, keyspace_name):
+    result = session.execute(schema_query, (keyspace_name,))
+    dicts = []
+    for a in result:
+        a = a.__dict__
+        dicts.append(a)
+    return dicts
+
+def add_columns():
+    pass
+
+def profile_tables(session, keyspace_name):
+    a = get_table_schemas(session, keyspace_name)
+    # b = add_columns(a)
+    c = add_table_size(a)
+    return c
 
 if __name__ == "__main__":
     session = setup()
-    profile_tables(session, keyspace_name)
+    dicts = profile_tables(session, keyspace_name)
+    generate_csv(dicts)
