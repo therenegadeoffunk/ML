83c70e6cce33bfb4419df06178c635d907510d5a
TheRenegadeOfFunk <therenegadeoffunk@gmail.com>
2016-11-05 14:41:47 +0900

    Created method for inserting text file into Collection
    Pretty crude, but it works. We have separate documents for each
    line of the file. Now we can worry about more fun stuff.

--- Java/MongoSVM/src/com/company/Main.java
+++ Java/MongoSVM/src/com/company/Main.java
@@ -1,10 +1,15 @@
 package com.company;
+import com.mongodb.MongoClient;
+
+import java.io.FileNotFoundException;
+import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 	// write your code here
-        TextData TD = new TextData();
+        MongoClient mongoClient = new MongoClient("localhost", 27017);
+        TextData TD = new TextData(mongoClient);
         TD.GetDataFromTextfile();
     }
 }
--- Java/MongoSVM/src/com/company/TextData.java
+++ Java/MongoSVM/src/com/company/TextData.java
@@ -1,20 +1,44 @@
 package com.company;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.stream.Stream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import com.mongodb.BasicDBObject;
+
+import java.util.List;
+import java.util.Scanner;
 
 public class TextData {
-    public TextData() {
+    private MongoClient mc;
+    private DBCollection c;
+
+    public TextData(MongoClient mongoClient) {
+        this.mc = mongoClient;
+        this.c = this.mc.getDB("testdb").getCollection("mail");
     }
 
-    public void GetDataFromTextfile() {
+    public void InsertLinesAsDocs(String PATH) throws FileNotFoundException {
+        Scanner s = new Scanner(new File(PATH));
+        while (s.hasNextLine()) {
+            List<String> f;
+            String r;
+            f = Arrays.asList(s.nextLine().split(","));
+            InsertLineAsDoc(f);
+        }
+        s.close();
+    }
+
+    public void InsertLineAsDoc(List LINE) {
+        BasicDBObject obj = new BasicDBObject("x", LINE.get(0))
+                .append("y", LINE.get(1));
+        this.c.insert(obj);
+    }
+
+    public void GetDataFromTextfile() throws FileNotFoundException {
         String PATH;
         PATH = "Data/svm_data_1.txt";
-        try (Stream<String> stream = Files.lines(Paths.get(PATH))) {
-            stream.forEach(System.out::println);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        InsertLinesAsDocs(PATH);
     }
 }
