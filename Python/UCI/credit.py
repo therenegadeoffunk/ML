786dd816f1c4985825dbd8749919083f6f817e20
agathathewitch <therenegadeoffunk@gmail.com>
2017-09-26 14:48:18 +0900

    First pass at logistic regression implementation
    Props to Andrew Ng's ML class and John Wittenaur for references.
    Translating my Octave code proved difficult, but fortunately
    there were a number of Python sources.
    
    This "just works," and as prediced leads to a pretty crappy
    result. Age and education level just are not very good predictors
    of credit defaulting.

--- Python/UCI/credit.py
+++ Python/UCI/credit.py
@@ -3,6 +3,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import scipy.optimize as opt
 
 data_path = 'training_set.csv'
 
@@ -12,7 +13,7 @@ df = pd.read_csv(data_path)
 
 reduced = df[['X3', 'X5', 'Y']].copy()
 viz = reduced.iloc[1:]
-viz = viz.head(n=50)
+viz = viz.head(n=500)
 # Convert the strings to numerical type
 
 viz.Y = pd.to_numeric(viz.Y, errors='coerce')
@@ -24,6 +25,8 @@ viz.X5 = pd.to_numeric(viz.X5, errors='coerce')
 pos = viz[viz['Y'].isin([1])]
 neg = viz[viz['Y'].isin([0])]
 
+# Make a scatter plot
+
 fig, ax = plt.subplots(figsize=(12,8))
 ax.scatter(pos['X5'], pos['X3'], s=50, c='b', marker='o', label='Defaulted')  
 ax.scatter(neg['X5'], neg['X3'], s=50, c='r', marker='x', label='Not_Defaulted')  
@@ -31,3 +34,51 @@ ax.legend()
 ax.set_xlabel('Age')  
 ax.set_ylabel('Education')
 plt.show()
+
+# Make a sigmoid function
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+# Make a logistic regression cost function
+
+def cost(theta, X, y):
+    theta = np.matrix(theta)
+    X = np.matrix(X)
+    y = np.matrix(y)
+    start = np.multiply(-y, np.log(sigmoid(X * theta.T)))
+    stop = np.multiply((1 - y), np.log(1 - sigmoid(X * theta.T)))
+    return np.sum(start - stop) / len(X)
+
+# Shape the X and y data and convert to np arrays
+
+viz.insert(0, 'Ones', 1)
+columns = viz.shape[1]
+X = viz.iloc[:,0:columns-1]
+y = viz.iloc[:,columns-1:columns]
+X = np.array(X.values)
+y = np.array(y.values)
+theta = np.zeros(3)
+
+# Function for computing gradient
+
+def gradient(theta, X, y):
+    theta = np.matrix(theta)
+    X = np.matrix(X)
+    y = np.matrix(y)
+
+    param = int(theta.ravel().shape[1])
+    grad = np.zeros(param)
+
+    err = sigmoid(X * theta.T) - y
+
+    for i in range(param):
+        val = np.multiply(err, X[:,i])
+        grad[i] = np.sum(val) / len(X)
+
+    return grad
+
+out = opt.fmin_tnc(func=cost, x0=theta, fprime=gradient, args=(X, y))
+final = cost(out[0], X, y)
+
+print final
